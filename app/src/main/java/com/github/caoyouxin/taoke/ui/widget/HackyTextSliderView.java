520daa573058e263bc5f4e4df7f50b425918bd92
jasontsang <jasontsang.icloud@me.com>
2017-10-24 10:54:15 +0800

    pages

---
+++ app/src/main/java/com/github/caoyouxin/taoke/ui/widget/HackyTextSliderView.java
@@ -0,0 +1,25 @@
+package com.github.caoyouxin.taoke.ui.widget;
+
+import android.content.Context;
+import android.view.View;
+
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.github.caoyouxin.taoke.R;
+
+/**
+ * Created by jasontsang on 10/24/17.
+ */
+
+public class HackyTextSliderView extends TextSliderView {
+    public HackyTextSliderView(Context context) {
+        super(context);
+    }
+
+    @Override
+    public View getView() {
+        View view = super.getView();
+        //hack to hide description
+        view.findViewById(R.id.description_layout).setBackgroundDrawable(null);
+        return view;
+    }
+}
