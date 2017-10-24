92a8c424907b30823f39ca0411899c781a861f3c
jasontsang <jasontsang.icloud@me.com>
2017-10-24 09:15:32 +0800

    pages

--- app/src/main/java/com/github/caoyouxin/taoke/ui/activity/TaoKeActivity.java
+++ app/src/main/java/com/github/caoyouxin/taoke/ui/activity/TaoKeActivity.java
@@ -1,12 +1,22 @@
 package com.github.caoyouxin.taoke.ui.activity;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.util.ArrayMap;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.github.caoyouxin.taoke.R;
+import com.github.caoyouxin.taoke.ui.fragment.AccountFragment;
+import com.github.caoyouxin.taoke.ui.fragment.ChartFragment;
+import com.github.caoyouxin.taoke.ui.fragment.DiscoverFragment;
+import com.github.caoyouxin.taoke.ui.fragment.MessageFragment;
 import com.github.gnastnosaj.boilerplate.ui.activity.BaseActivity;
+
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,14 +31,16 @@ public class TaoKeActivity extends BaseActivity {
     @BindView(R.id.tab_discover)
     LinearLayout tabDiscover;
 
-    @BindView(R.id.tab_chat)
-    LinearLayout tabChat;
+    @BindView(R.id.tab_chart)
+    LinearLayout tabChart;
 
     @BindView(R.id.tab_message)
     LinearLayout tabMessage;
 
     @BindView(R.id.tab_account)
     LinearLayout tabAccount;
+
+    private Map<Integer, Fragment> pages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,11 +53,13 @@ public class TaoKeActivity extends BaseActivity {
         initSystemBar();
 
         decorateTab(tabDiscover);
+
+        showTabContent(tabDiscover.getId());
     }
 
-    @OnClick({R.id.tab_discover, R.id.tab_chat, R.id.tab_message, R.id.tab_account})
+    @OnClick({R.id.tab_discover, R.id.tab_chart, R.id.tab_message, R.id.tab_account})
     protected void onTabClick(View view) {
-        LinearLayout[] tabs = new LinearLayout[]{tabDiscover, tabChat, tabMessage, tabAccount};
+        LinearLayout[] tabs = new LinearLayout[]{tabDiscover, tabChart, tabMessage, tabAccount};
         for (LinearLayout tab : tabs) {
             tab.animate().scaleX(1.0f).scaleY(1.0f);
             ((TextView) tab.getChildAt(0)).setTextColor(getResources().getColor(R.color.grey_600));
@@ -53,11 +67,28 @@ public class TaoKeActivity extends BaseActivity {
         }
 
         decorateTab((LinearLayout) view);
+        showTabContent(view.getId());
     }
 
     private void decorateTab(LinearLayout tab) {
         tab.animate().scaleX(1.1f).scaleY(1.1f);
         ((TextView) tab.getChildAt(0)).setTextColor(getResources().getColor(R.color.colorPrimary));
         ((TextView) tab.getChildAt(1)).setTextColor(getResources().getColor(R.color.colorPrimary));
+    }
+
+    private void showTabContent(int tabId) {
+        if (pages == null) {
+            pages = new ArrayMap<>();
+            pages.put(tabDiscover.getId(), new DiscoverFragment());
+            pages.put(tabChart.getId(), new ChartFragment());
+            pages.put(tabMessage.getId(), new MessageFragment());
+            pages.put(tabAccount.getId(), new AccountFragment());
+        }
+        FragmentManager fm = getSupportFragmentManager();
+        if (pages.containsKey(tabId)) {
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.tab_content, pages.get(tabId));
+            ft.commit();
+        }
     }
 }
---
+++ app/src/main/java/com/github/caoyouxin/taoke/ui/fragment/AccountFragment.java
@@ -0,0 +1,35 @@
+package com.github.caoyouxin.taoke.ui.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.caoyouxin.taoke.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by jasontsang on 10/24/17.
+ */
+
+public class AccountFragment extends Fragment {
+
+    View rootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (rootView == null) {
+            rootView = inflater.inflate(R.layout.fragment_account, container, false);
+            ButterKnife.bind(this, rootView);
+
+            if (savedInstanceState != null) {
+                //restore
+            }
+        }
+        return rootView;
+    }
+}
