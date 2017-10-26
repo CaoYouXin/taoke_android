56ecbedbb5c903d101a7fb74f31b97d1a206f3e9
曹又心 <c_lisheng@163.com>
2017-10-26 09:00:45 +0800

    my layout using ion icons.

--- app/src/main/java/com/github/caoyouxin/taoke/ui/fragment/AccountFragment.java
+++ app/src/main/java/com/github/caoyouxin/taoke/ui/fragment/AccountFragment.java
@@ -1,14 +1,26 @@
 package com.github.caoyouxin.taoke.ui.fragment;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StrikethroughSpan;
+import android.text.style.StyleSpan;
+import android.text.style.URLSpan;
+import android.text.style.UnderlineSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.caoyouxin.taoke.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -16,6 +28,9 @@ import butterknife.ButterKnife;
  */
 
 public class AccountFragment extends Fragment {
+
+    @BindView(R.id.account_id)
+    TextView accountId;
 
     View rootView;
 
@@ -29,7 +44,16 @@ public class AccountFragment extends Fragment {
             if (savedInstanceState != null) {
                 //restore
             }
+
+            this.initAccountId();
         }
         return rootView;
+    }
+
+    private void initAccountId() {
+        SpannableString span = new SpannableString("账户ID:  954382491\n淘客954382491");
+        span.setSpan(new RelativeSizeSpan(1.36f), 17, 28, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        span.setSpan(new ForegroundColorSpan(Color.DKGRAY), 0, 16, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        accountId.setText(span);
     }
 }
