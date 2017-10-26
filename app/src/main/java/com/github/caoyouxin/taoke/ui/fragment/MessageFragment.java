93ad2611d37fc7a3c4fcebc321f485d409689e51
曹又心 <c_lisheng@163.com>
2017-10-26 11:14:36 +0800

    msg entry

--- app/src/main/java/com/github/caoyouxin/taoke/ui/fragment/MessageFragment.java
+++ app/src/main/java/com/github/caoyouxin/taoke/ui/fragment/MessageFragment.java
@@ -1,14 +1,23 @@
 package com.github.caoyouxin.taoke.ui.fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.github.caoyouxin.taoke.R;
 
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 /**
@@ -16,6 +25,9 @@ import butterknife.ButterKnife;
  */
 
 public class MessageFragment extends Fragment {
+
+    @BindView(R.id.global_msg)
+    TextView globalMsg;
 
     View rootView;
 
@@ -29,7 +41,16 @@ public class MessageFragment extends Fragment {
             if (savedInstanceState != null) {
                 //restore
             }
+
+            this.initAccountId();
         }
         return rootView;
+    }
+
+    private void initAccountId() {
+        SpannableString span = new SpannableString("动态\n淘宝客新模式");
+        span.setSpan(new RelativeSizeSpan(1.36f), 0, 2, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        span.setSpan(new ForegroundColorSpan(Color.DKGRAY), 3, 9, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        globalMsg.setText(span);
     }
 }
