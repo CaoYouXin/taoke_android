224e99518cb1a46d12aae0baad510082ee477e0d
jasontsang <jasontsang.icloud@me.com>
2017-10-27 17:03:09 +0800

    product

--- app/src/main/java/com/github/caoyouxin/taoke/model/BrandItem.java
+++ app/src/main/java/com/github/caoyouxin/taoke/model/BrandItem.java
@@ -1,10 +1,44 @@
 package com.github.caoyouxin.taoke.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by jasontsang on 10/25/17.
  */
 
-public class BrandItem {
+public class BrandItem implements Parcelable {
     public int type;
     public String thumb;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.type);
+        dest.writeString(this.thumb);
+    }
+
+    public BrandItem() {
+    }
+
+    protected BrandItem(Parcel in) {
+        this.type = in.readInt();
+        this.thumb = in.readString();
+    }
+
+    public static final Parcelable.Creator<BrandItem> CREATOR = new Parcelable.Creator<BrandItem>() {
+        @Override
+        public BrandItem createFromParcel(Parcel source) {
+            return new BrandItem(source);
+        }
+
+        @Override
+        public BrandItem[] newArray(int size) {
+            return new BrandItem[size];
+        }
+    };
 }
